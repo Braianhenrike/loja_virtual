b308cc7909ce1ea24898aa36e8373abb603b5d1b
Braian Henrique <80363931+Braianhenrike@users.noreply.github.com>
2023-04-04 19:46:07 -0300

    Criada a classe categoria do produto (CategoriaProduto)

---
+++ src/main/java/lojaVirtual/model/CategoriaProduto.java
@@ -0,0 +1,12 @@
+package lojaVirtual.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class CategoriaProduto implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+}
